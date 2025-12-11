db2b73a92f0c4e099f5a14dcd7f8b1651f0007a8
GeorgeFerguson2910 <georgewferguson@btinternet.com>
2025-12-11 09:44:16 +0000

    fixed build command

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,11 @@
 const getAPI = () => {
   const isLocalhost = window.location.hostname === "localhost";
   const isStaging = window.location.href.includes("staging");
+  const isProduction = window.location.href.includes("production");
 
   if (isLocalhost) return "http://localhost:4000"; // local server
   if (isStaging) return "";                        // staging URL here
+  if (isProduction) return "https://siot-server.onrender.com";                     // production URL here
 
   return "";                                       // production URL here
 };
